9eabb499357aa5d01def93219a97173e40eda6f4
Aleksei Kishinskii <a.kishinskii@mercuryo.io>
2025-06-15 19:36:24 +0200

    Initial Commit

---
+++ TRPG Encounter/TRPG_EncounterApp.swift
@@ -0,0 +1,32 @@
+//
+//  TRPG_EncounterApp.swift
+//  TRPG Encounter
+//
+//  Created by Aleksei Kishinskii on 15. 6. 2025..
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TRPG_EncounterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TRPG EncounterTests/TRPG_EncounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  TRPG_EncounterTests.swift
+//  TRPG EncounterTests
+//
+//  Created by Aleksei Kishinskii on 15. 6. 2025..
+//
+
+import Testing
+@testable import TRPG_Encounter
+
+struct TRPG_EncounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
